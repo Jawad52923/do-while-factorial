d7c6e057bfb1e56a78083107badb110dc947434b
Your Name <you@example.com>
2020-12-19 19:51:51 +0500

    first commit

---
+++ do-while-factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace do_while_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+
+            do
+            {
+                factorial *= n;
+                n--;
+            } while (n > 0);
+            Console.WriteLine("n! = {0}", factorial);
+        }
+    }
+}
